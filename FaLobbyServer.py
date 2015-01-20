b669171ba53c4a8c774df8471c330ea16c976f4e
Michael Sondergaard <msond@cs.au.dk>
2015-01-20 14:35:12 +0100

    Cleanup FaLobbyServer.py

--- FaLobbyServer.py
+++ FaLobbyServer.py
@@ -102,7 +102,7 @@ class FALobbyServer(QtNetwork.QTcpServer):
         jsonToSend["uid"] = game.getuuid()
         jsonToSend["title"] = game.getGameName()
         jsonToSend["state"] = game.getLobbyState()
-        jsonToSend["featured_mod"]= game.getGamemod()
+        jsonToSend["featured_mod"] = game.getGamemod()
         jsonToSend["featured_mod_versions"] = game.getGamemodVersion()
         
         jsonToSend["sim_mods"] = game.getSimMods()
@@ -118,8 +118,8 @@ class FALobbyServer(QtNetwork.QTcpServer):
         teams = game.getTeamsAssignements()
 
         teamsToSend = {}
-        for k, v in teams.iteritems() :
-            if len(v) != 0 :
+        for k, v in teams.iteritems():
+            if len(v) != 0:
                 teamsToSend[k] = v
 
 
@@ -134,103 +134,49 @@ class FALobbyServer(QtNetwork.QTcpServer):
 
     @timed
     def dirtyGameCheck(self):
-        #self.logger.debug("dirtyGameCheck start")
-        if time.time() - self.lastDirty > 5.2 and self.skippedDirty < 2 :
+        if time.time() - self.lastDirty > 5.2 and self.skippedDirty < 2:
             self.logger.debug("skipping")
-            #the server is probably laggin, we a beat
             self.lastDirty = time.time()
-            self.skippedDirty = self.skippedDirty + 1 
+            self.skippedDirty += 1
         
         else :
             self.lastDirty = time.time()
             self.skippedDirty = 0
             reply = QtCore.QByteArray()
             
-            for uid in self.dirtyGameList :
+            for uid in self.dirtyGameList:
         
                 game = self.parent.games.getGameByUuid(uid)
-                if game != None :
-                    container =  self.parent.games.getGameContainer(game)
-                    if container.isListable() == True or container.isPrivateBeta() == False :
+                if game is not None:
+                    container = self.parent.games.getGameContainer(game)
+                    if container.isListable() or not container.isPrivateBeta():
                         reply.append(self.prepareBigJSON(self.jsonGame(game)))
-                else :
-                    # the current uid doesn't have an associated game. It shouldn't happen, and the game is probably invalid.
-                    # We send "empty" info to the players to be sure that the game is removed on his side.
-                    jsonToSend = {}
-                    jsonToSend["command"] = "game_info"
-                    jsonToSend["uid"] = uid
-                    jsonToSend["title"] = "unknown"
-                    jsonToSend["state"] = "closed"
-                    jsonToSend["featured_mod"]= "unknown"
-                    jsonToSend["featured_mod_versions"] = {}
-                    jsonToSend["sim_mods"]= []
-                    jsonToSend["mapname"] = "unknown"
-                    jsonToSend["host"] = "unknown"
-                    jsonToSend["num_players"] = 0
-                    jsonToSend["game_type"] = "unknown"
-                    jsonToSend["game_time"] = 0
-                    jsonToSend["max_players"] = 0
-                    jsonToSend["teams"] = {}
-                    jsonToSend["options"] = []
-                    
+                else:
+                    # If no game was found, send a bogus object to ensure client state updates
+                    jsonToSend = {"command": "game_info",
+                                  "uid": uid,
+                                  "title": "unknown",
+                                  "state": "closed",
+                                  "featured_mod": "unknown",
+                                  "featured_mod_versions": {},
+                                  "sim_mods": [],
+                                  "mapname": "unknown",
+                                  "host": "unknown",
+                                  "num_players": 0,
+                                  "game_type": "unknown",
+                                  "game_time": 0,
+                                  "max_players": 0,
+                                  "teams": {},
+                                  "options": []}
+
                     reply.append(self.prepareBigJSON(jsonToSend))
                                        
-                if uid in self.dirtyGameList : 
+                if uid in self.dirtyGameList:
                     self.dirtyGameList.remove(uid)
-            
 
-            for lobby in self.recorders :
+            for lobby in self.recorders:
                     lobby.sendArray(reply)
                     
-
-
-    @timed
-    def preparePacket(self, action, *args, **kwargs) :
-        reply = QtCore.QByteArray()
-        stream = QtCore.QDataStream(reply, QtCore.QIODevice.WriteOnly)
-        stream.setVersion(QtCore.QDataStream.Qt_4_2)
-        stream.writeUInt32(0)
-        
-        stream.writeQString(action)
-        
-        for arg in args :
-            if type(arg) is LongType :
-                stream.writeQString(str(arg))
-            elif type(arg) is IntType:
-                stream.writeInt(arg)
-            elif isinstance(arg, basestring):                       
-                stream.writeQString(arg)                  
-            elif type(arg) is StringType  :
-                stream.writeQString(arg)
-            elif type(arg) is FloatType:
-                stream.writeFloat(arg)
-            elif type(arg) is ListType:
-                stream.writeQString(str(arg))
-        
-        stream.device().seek(0)
-        
-        stream.writeUInt32(reply.size() - 4)  
-        
-        return reply  
-
-    @timed
-    def prepareBigJSON(self, data_dictionary):
-        '''
-        Simply dumps a dictionary into a string and feeds it into the QTCPSocket
-        '''
-
-        data_string = ""
-        try :
-            data_string = json.dumps(data_dictionary)
-        except :
-            return
-
-        return self.preparePacket(data_string)
-    
-    @timed
-    def addSocketToDelete(self, socket):     
-        self.socketToDelete.append([time.time(), socket])
-            
     @timed
     def dump_garbage(self):
 
