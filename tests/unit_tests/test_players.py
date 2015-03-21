cb4989ac4184c77a07ee75225e98d6043e66cede
Michael Sondergaard <msond@cs.au.dk>
2015-03-21 20:28:05 +0100

    Test assignment of rating from tuple

--- tests/unit_tests/test_players.py
+++ tests/unit_tests/test_players.py
@@ -1,5 +1,16 @@
+from trueskill import Rating
 from src.abc.faction import Faction
 from src.players import Player
+
+
+def test_ratings():
+    p = Player('Schroedinger')
+    p.global_rating = (1500, 20)
+    assert p.global_rating == Rating(1500, 20)
+    p.global_rating = Rating(1700, 20)
+    assert p.global_rating == Rating(1700, 20)
+    p.ladder_rating = (1200, 20)
+    assert p.ladder_rating == Rating(1200, 20)
 
 
 def test_faction():
