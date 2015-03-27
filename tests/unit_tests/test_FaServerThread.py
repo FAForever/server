aab2394e5579a63b5e447380efb2b3e249fa2573
Dragonfire <bastian616-git@yahoo.de>
2015-03-27 13:12:49 +0100

    Test for non-ASCII characters in hosted games

--- tests/unit_tests/test_FaServerThread.py
+++ tests/unit_tests/test_FaServerThread.py
@@ -21,6 +21,20 @@ def test_game_info():
         'options':  []
     }
 
+@pytest.fixture()
+def test_game_info_invalid():
+    return {
+        'title': 'Tittle with non ASCI char \xc3',
+        'gameport': '8000',
+        'access': 'public',
+        'mod': 'faf',
+        'version': None,
+        'mapname': 'scmp_007',
+        'password': None,
+        'lobby_rating': 1,
+        'options':  []
+    }
+
 
 @pytest.fixture(scope='function')
 def connected_socket():
@@ -39,5 +53,25 @@ def mock_lobby_server(db):
 def test_command_game_host_calls_host_game(connected_socket,
                                            mock_lobby_server,
                                            test_game_info):
+    mock_lobby_server.games.create_game = mock.Mock()
     server_thread = LobbyConnection(connected_socket, mock_lobby_server)
     server_thread.command_game_host(test_game_info)
+    mock_lobby_server.games.create_game\
+        .assert_called_with(test_game_info['access'],
+                            test_game_info['mod'],
+                            server_thread.player,
+                            test_game_info['title'],
+                            test_game_info['gameport'],
+                            test_game_info['mapname'],
+                            test_game_info['version'])
+
+
+def test_command_game_host_calls_host_game_invalid_title(connected_socket,
+                                           mock_lobby_server,
+                                           test_game_info_invalid):
+    server_thread = LobbyConnection(connected_socket, mock_lobby_server)
+    server_thread.sendJSON = mock.Mock()
+    mock_lobby_server.games.create_game = mock.Mock()
+    server_thread.command_game_host(test_game_info_invalid)
+    assert mock_lobby_server.games.create_game.mock_calls == []
+    server_thread.sendJSON.assert_called_with(dict(command="notice", style="error", text="Non-ascii characters in game name detected."))
