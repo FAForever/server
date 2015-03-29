69d7c7ecff7cbfe1d4df7c2cc5d62e85d821e108
Michael Sondergaard <msond@cs.au.dk>
2015-03-29 23:18:37 +0200

    Refactor ladderGame.trueskillUpdate

--- src/games/ladderGame.py
+++ src/games/ladderGame.py
@@ -87,113 +87,101 @@ class ladder1V1Game(Game):
             new_ratings = self.compute_rating()
             self.persist_rating_change_stats(new_ratings, rating='ladder1v1')
 
-    def specialEnding(self, logger, db, players):
-        if len(self.invalidPlayers) == 2:
-            self.setInvalid("Scores not validated. Possible reason: Disconnection between players.")
-        
-        if self.valid:
-            if self.isDraw():
-                query = QSqlQuery(db)
-                queryStr = ("SELECT id FROM table_map WHERE filename LIKE '%"+self.mapName+"%'")
+    def is_winner(self, player):
+        return self.get_army_result(self.get_player_option(player.id, 'Army')) > 0
+
+    def on_game_end(self):
+        super().on_game_end()
+        if not self.valid:
+            return
+        if self.isDraw():
+            query = QSqlQuery(self.db)
+            queryStr = ("SELECT id FROM table_map WHERE filename LIKE '%" + self.mapName + "%'")
+            query.exec_(queryStr)
+            while query.next():
+                mapId = query.value(0)
+
+                queryStr = ("UPDATE table_map_features set num_draws = (num_draws +1) WHERE map_id LIKE " + str(mapId))
+                query = QSqlQuery(self.db)
                 query.exec_(queryStr)
+            return
 
-                if  query.size() == 1:
-                    query.first()
-                    mapId = query.value(0)
-                
-                    queryStr = ("UPDATE table_map_features set num_draws = (num_draws +1) WHERE map_id LIKE " + str(mapId))
-                    query.exec_(queryStr)
+        # And for the ladder !
+        evenLeague = True
+        maxleague = max(iter(self.leagues.items()), key=operator.itemgetter(1))[1]
+        if len(set(self.leagues.values())) != 1:
+            evenLeague = False
 
-            tsresults = self.computeResults1v1()
-            tsplayers = self.getTrueSkill1v1Players()
-            self.trueSkillUpdate(tsresults, tsplayers, logger, db, players, playerFnc="setladder1v1Rating" ,table="ladder1v1_rating", winner=True, sendScore=False)
-        
-            # and for the ladder !
-            evenLeague = True
-            maxleague = max(iter(self.leagues.items()), key=operator.itemgetter(1))[1]
-            if len(set(self.leagues.values())) != 1:
-                evenLeague = False
-                
-            if not self.isDraw():
-                query = QSqlQuery(db)
-                for player in self.gameResult :
-                    if self.isWinner(player) :
-                        # if not even league:
-                        scoreToAdd = 1
-                        if not evenLeague:
-                            if self.leagues[player] == maxleague:
-                                scoreToAdd = 0.5
-                            else :
-                                scoreToAdd = 1.5
-                            
-                        query.prepare("UPDATE %s SET score = (score + ?) WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                        query.addBindValue(scoreToAdd)
-                        query.addBindValue(player)
-                        query.exec_()
-                        logger.debug(query.executedQuery())
-                    else:
-                        # if not even league:
-                        scoreToRemove = 0.5
-                        if not evenLeague :
-                            if self.leagues[player] == maxleague:
-                                scoreToRemove = 1
-                            else :
-                                scoreToRemove = 0
+        query = QSqlQuery(self.db)
+        for player in self.players:
+            if self.is_winner(player):
+                # if not even league:
+                scoreToAdd = 1
+                if not evenLeague:
+                    if self.leagues[player] == maxleague:
+                        scoreToAdd = 0.5
+                    else :
+                        scoreToAdd = 1.5
 
-                        query.prepare("UPDATE %s SET score = GREATEST(0,(score - ?)) WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                        query.addBindValue(scoreToRemove)
-                        query.addBindValue(player)
-                        query.exec_()
-                        logger.debug(query.executedQuery()) 
-                
-                    #check if the user must be promoted
-                    query.prepare("SELECT league, score FROM %s WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                    query.addBindValue(player)
+                query.prepare("UPDATE %s SET score = (score + ?) "
+                              "WHERE `idUser` = ?" % self.parent.season)
+                query.addBindValue(scoreToAdd)
+                query.addBindValue(player.id)
+                query.exec_()
+                self._logger.debug(query.executedQuery())
+            else:
+                # if not even league:
+                scoreToRemove = 0.5
+                if not evenLeague :
+                    if self.leagues[player] == maxleague:
+                        scoreToRemove = 1
+                    else :
+                        scoreToRemove = 0
+
+                query.prepare("UPDATE %s SET score = GREATEST(0,(score - ?))"
+                              "WHERE `idUser` = ?" % self.parent.season)
+                query.addBindValue(scoreToRemove)
+                query.addBindValue(player.id)
+                query.exec_()
+                self._logger.debug(query.executedQuery())
+
+            #check if the user must be promoted
+            query.prepare("SELECT league, score FROM %s"
+                          "WHERE `idUser` = ?" % self.parent.season)
+            query.addBindValue(player.id)
+            query.exec_()
+            if query.size() != 0:
+                query.first()
+                pleague = int(query.value(0))
+                pscore = float(query.value(1))
+                # Minimum scores, by league, to move to next league
+                league_incr_min = {1: 50, 2: 75, 3: 100, 4: 150}
+                if pleague in league_incr_min and pscore > league_incr_min[pleague]:
+                    query.prepare("UPDATE %s SET league = league+1, score = 0"
+                                  "WHERE `idUser` = ?" % self.parent.season)
+                    query.addBindValue(player.id)
                     query.exec_()
-                    if query.size() != 0:
-                        query.first()
-                        pleague = int(query.value(0))
-                        pscore = float(query.value(1))
-                        if pleague == 1 and pscore > 50:
-                            query.prepare("UPDATE %s SET league = league+1, score = 0 WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                            query.addBindValue(player)
-                            query.exec_()
-                        elif pleague == 2 and pscore > 75:                      
-                            query.prepare("UPDATE %s SET league = league+1, score = 0 WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                            query.addBindValue(player)
-                            query.exec_()
-                        elif pleague == 3 and pscore > 100:                      
-                            query.prepare("UPDATE %s SET league = league+1, score = 0 WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                            query.addBindValue(player)
-                            query.exec_()
-                        elif pleague == 4 and pscore > 150:                      
-                            query.prepare("UPDATE %s SET league = league+1, score = 0 WHERE `idUser` = (SELECT id FROM login WHERE login.login = ?)" % self.parent.season)
-                            query.addBindValue(player)
-                            query.exec_()
 
-                        for p in players.players() :
-                            if str(p.getLogin()) == str(player) :
-                                query.prepare("SELECT score, league FROM %s WHERE idUser = ?" % self.parent.season)
-                                query.addBindValue(p.getId())
-                                query.exec_()
-                                if  query.size() > 0:
-                                    query.first()
-                                    score = float(query.value(0))
-                                    league = int(query.value(1))
-                                    
-                                    query.prepare("SELECT name, `limit` FROM `ladder_division` WHERE `league` = ? AND `limit` >= ? ORDER BY `limit` ASC LIMIT 1")
-                                    query.addBindValue(league)
-                                    query.addBindValue(score)
-                                    query.exec_()
-                                    if query.size() > 0:
-                                        query.first()
-                                        p.setLeague(league)
-                                        p.division = str(query.value(0))
-        else:
-            tsplayers = self.trueSkillPlayers
-            for playerTS in tsplayers : 
-                name = playerTS.getPlayer()
-                self.sendMessageToPlayers(players, name, self.getInvalidReason())
+                for p in self.players:
+                    query.prepare("SELECT score, league FROM %s WHERE idUser = ?" % self.parent.season)
+                    query.addBindValue(p.id)
+                    query.exec_()
+                    if query.size() > 0:
+                        query.first()
+                        score = float(query.value(0))
+                        league = int(query.value(1))
+
+                        query.prepare("SELECT name, `limit` "
+                                      "FROM `ladder_division` "
+                                      "WHERE `league` = ? AND `limit` >= ?"
+                                      "ORDER BY `limit` ASC LIMIT 1")
+                        query.addBindValue(league)
+                        query.addBindValue(score)
+                        query.exec_()
+                        if query.size() > 0:
+                            query.first()
+                            p.setLeague(league)
+                            p.division = str(query.value(0))
 
     def addPlayerToJoin(self, player):
         self.playerToJoin = player
