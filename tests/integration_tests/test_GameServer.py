ff560cd8b5cc6088b798e3ee3bf27575d2b32ca6
Michael Sondergaard <msond@cs.au.dk>
2015-04-17 20:07:08 +0200

    Cleanup test_GameServer

--- tests/integration_tests/test_GameServer.py
+++ tests/integration_tests/test_GameServer.py
@@ -1,10 +1,7 @@
-from asyncio import coroutine, sleep
 import asyncio
-import time
 
 from mock import call
 import pytest
-import ujson
 import config
 
 from server import run_game_server
@@ -14,17 +11,6 @@ from tests.integration_tests.testclient import TestGPGClient
 slow = pytest.mark.slow
 
 TEST_ADDRESS = ('127.0.0.1', 8000)
-
-
-@coroutine
-def wait_call(mock, call, timeout=0.5):
-    start_time = time.time()
-    yield from asyncio.sleep(0.1)
-    while time.time() - start_time < timeout:
-        if call in mock.mock_calls:
-            return True
-        yield from sleep(0.1)
-    assert call in mock.mock_calls
 
 @asyncio.coroutine
 @slow
