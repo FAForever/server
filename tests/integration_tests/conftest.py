b86819bd934eca5e15f3b85dd99858b5ea9bddd7
Michael Sondergaard <msond@cs.au.dk>
2015-08-12 11:54:15 +0100

    Overwrite client_version_info

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -18,7 +18,9 @@ def mock_db_pool(mock_db_pool):
 
 @pytest.fixture
 def mock_players():
-    return mock.create_autospec(PlayerService(mock_db_pool))
+    m = mock.create_autospec(PlayerService(mock_db_pool))
+    m.client_version_info = (0, None)
+    return m
 
 @pytest.fixture
 def mock_games(mock_players, db):
