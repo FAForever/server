e58630b09181ceb7dbe110993573571664f8be75
Sheeo <sheeo@faforever.com>
2015-08-25 15:50:05 +0200

    Reformat custom_game

--- server/games/custom_game.py
+++ server/games/custom_game.py
@@ -1,17 +1,14 @@
-from copy import deepcopy
 import time
 
 from .game import Game, ValidityState
 from server.abc.base_game import InitMode
 from server.decorators import with_logger
 
+
 @with_logger
 class CustomGame(Game):
     init_mode = InitMode.NORMAL_LOBBY
 
-    def __init__(self, id, parent):
-        super(self.__class__, self).__init__(id, parent)
-  
     def rate_game(self):
         limit = len(self.players) * 60
         if time.time() - self.launched_at < limit:
