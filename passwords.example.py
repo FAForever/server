04a38efe60dae12bb41a288f9d0e15b370ec646f
Sheeo <sheeo@faforever.com>
2015-09-23 18:25:44 +0200

    Update passwords.example.py for docker

--- passwords.example.py
+++ passwords.example.py
@@ -1,9 +1,11 @@
+import os
+
 PRIVATE_KEY = None
-DB_SERVER = "localhost"
-DB_PORT = 3306
-DB_LOGIN = "faf_lobby"
-DB_PASSWORD = "banana"
-DB_NAME = "faf_test"
+DB_SERVER = os.getenv("DB_PORT_3306_TCP_ADDR", "localhost")
+DB_PORT = int(os.getenv("DB_PORT_3306_TCP_PORT", "3306"))
+DB_LOGIN = os.getenv("FAF_DB_LOGIN", "root")
+DB_PASSWORD = os.getenv("DB_ENV_MYSQL_ROOT_PASSWORD", "banana")
+DB_NAME = os.getenv("FAF_DB_NAME", "faf_test")
 
 CHALLONGE_KEY = "challonge_key"
 CHALLONGE_USER = "challonge_user"
