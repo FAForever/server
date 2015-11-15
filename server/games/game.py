91683ddf8dabb1173f8adb39c06e149f35d1877f
Sheeo <sheeo@faforever.com>
2015-11-15 17:52:10 +0100

    Use mapname for client message

--- server/games/game.py
+++ server/games/game.py
@@ -586,7 +586,7 @@ class Game(BaseGame):
             "featured_mod": self.game_mode,
             "featured_mod_versions": self.getGamemodVersion(),
             "sim_mods": self.mods,
-            "map_file_path": (self.map_file_path or "").lower(),
+            "mapname": (self.map_file_path or "").lower(),
             "host": self.host.login if self.host else '',
             "num_players": len(self.players),
             "game_type": self.gameType,
