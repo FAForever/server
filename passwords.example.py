eae29debfdae58038baa2f2daed764240b798392
Sheeo <sheeo@faforever.com>
2016-02-01 13:06:17 +0100

    Change environment name

--- passwords.example.py
+++ passwords.example.py
@@ -4,7 +4,7 @@ PRIVATE_KEY = None
 DB_SERVER = os.getenv("DB_PORT_3306_TCP_ADDR", "localhost")
 DB_PORT = int(os.getenv("DB_PORT_3306_TCP_PORT", "3306"))
 DB_LOGIN = os.getenv("FAF_DB_LOGIN", "root")
-DB_PASSWORD = os.getenv("DB_ENV_MYSQL_ROOT_PASSWORD", "banana")
+DB_PASSWORD = os.getenv("FAF_DB_PASSWORD", "banana")
 DB_NAME = os.getenv("FAF_DB_NAME", "faf_test")
 
 CHALLONGE_KEY = "challonge_key"
