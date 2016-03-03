c9acf12ca8e87ff66a33d22fee6ffb6133ec8589
Sheeo <sheeo@faforever.com>
2016-03-03 03:09:48 +0100

    Allow arbitrary args to CoopGame

--- server/games/coop.py
+++ server/games/coop.py
@@ -1,11 +1,13 @@
 from server.abc.base_game import InitMode
+
 from .game import Game, ValidityState
+
 
 class CoopGame(Game):
     """Class for coop game"""
     init_mode = InitMode.NORMAL_LOBBY
 
-    def __init__(self, id, parent = None):
-        super(self.__class__, self).__init__(id, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.validity = ValidityState.COOP_NOT_RANKED
