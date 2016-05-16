0c7458133fc6e181b807cf137aebe0616ea0a49d
Christian Schmidt <brutus5000@gmx.net>
2016-05-16 22:03:37 +0200

    Login data: test user passwords now get correctly hashed

--- tests/data/db-fixtures.sql
+++ tests/data/db-fixtures.sql
@@ -2,9 +2,9 @@ use faf_test;
 
 -- Login table
 delete from login;
-insert into login (id, login, email, password) values (1, 'test', 'test@example.com', 'test_password');
-insert into login (id, login, email, password) values (2, 'Dostya', 'dostya@cybran.example.com', 'vodka');
-insert into login (id, login, email, password) values (3, 'Rhiza', 'rhiza@aeon.example.com', 'puff_the_magic_dragon');
+insert into login (id, login, email, password) values (1, 'test', 'test@example.com', SHA2('test_password', 256));
+insert into login (id, login, email, password) values (2, 'Dostya', 'dostya@cybran.example.com', SHA2('vodka', 256));
+insert into login (id, login, email, password) values (3, 'Rhiza', 'rhiza@aeon.example.com', SHA2('puff_the_magic_dragon', 256));
 
 -- global rating
 delete from global_rating;
