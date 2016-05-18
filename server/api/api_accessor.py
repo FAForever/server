FAForever/server

commit c12c76c0549bcedf9b382378bd0a9220ccc1b0bd

Update code to be compatible with OAuth2client 2.0

server/api/api_accessor.py
@@ -2,7 +2,7 @@ import json
 from functools import partial
 import asyncio
 from httplib2 import Http
-from oauth2client.client import SignedJwtAssertionCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 from passwords import API_TOKEN_URI, API_BASE_URL
 
 
@@ -26,8 +26,9 @@ class ApiAccessor:
         return result
 
     def http(self, sub=None):
-        credentials = SignedJwtAssertionCredentials('faf-server', self.private_key,
-                                                    'write_achievements write_events',
-                                                    sub=sub,
-                                                    token_uri=API_TOKEN_URI)
+        credentials = ServiceAccountCredentials.from_p12_keyfile(
+            'faf-server',
+            'faf-server.pem',
+            scopes='write_achievements write_events'
+        )
         return credentials.authorize(Http())

commit 8cba3c3454a64377d30720ff30703ebf72174660

Fix OAuth access to faf-api which has been broken with c12c76c

server/api/api_accessor.py
@@ -8,8 +8,12 @@ from server.config import API_TOKEN_URI, API_BASE_URL
 
 class ApiAccessor:
     def __init__(self):
-        with open("faf-server.pem", "rb") as f:
-            self.private_key = f.read()
+        self._service_account_credentials = ServiceAccountCredentials.from_p12_keyfile(
+            'faf-server',
+            'faf-server.pem',
+            scopes='write_achievements write_events'
+        )
+        self._service_account_credentials.token_uri = API_TOKEN_URI
 
     async def api_get(self, path, player_id):
         loop = asyncio.get_event_loop()
@@ -26,9 +30,5 @@ class ApiAccessor:
         return result
 
     def http(self, sub=None):
-        credentials = ServiceAccountCredentials.from_p12_keyfile(
-            'faf-server',
-            'faf-server.pem',
-            scopes='write_achievements write_events'
-        )
+        credentials = self._service_account_credentials.create_delegated(sub)
         return credentials.authorize(Http())