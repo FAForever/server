b6961687c03e7a1047fc8c47b13ad3d3cfed81fe
Crotalus <Crotalus@users.noreply.github.com>
2016-10-11 22:55:20 +0200

    Add test for partial/corrupt stats

--- tests/unit_tests/test_game.py
+++ tests/unit_tests/test_game.py
@@ -468,6 +468,27 @@ async def test_report_army_stats_sends_stats_for_defeated_player(game: Game):
 
     game._game_stats_service.process_game_stats.assert_called_once_with(players[1], game, stats)
 
+async def test_partial_stats_not_affecting_rating_persistence(custom_game, event_service, achievement_service):
+    from server.stats.game_stats_service import GameStatsService
+    game = custom_game
+    game._game_stats_service = GameStatsService(event_service, achievement_service)
+    game.state = GameState.LOBBY
+    players = add_players(game, 2)
+    game.set_player_option(players[0].id, 'Team', 2)
+    game.set_player_option(players[1].id, 'Team', 3)
+    old_mean = players[0].global_rating[0]
+
+    await game.launch()
+    game.launched_at = time.time()-60*60
+    await game.add_result(0, 0, 'victory', 10)
+    await game.add_result(0, 1, 'defeat', -10)
+    await game.report_army_stats({'stats': {'Player 1': {}}})
+    await game.on_game_end()
+
+    assert game.validity is ValidityState.VALID
+    assert players[0].global_rating[0] > old_mean
+
+
 async def test_players_exclude_observers(game: Game):
     game.state = GameState.LOBBY
     players = add_players(game, 2)
