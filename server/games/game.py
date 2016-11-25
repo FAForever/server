58a9135d5e4924ee4fdce24323ef8d74b869f147
Michel Jung <michel.jung89@gmail.com>
2016-11-25 02:22:12 +0100

    #252 Process army stats only if army result is available

--- server/games/game.py
+++ server/games/game.py
@@ -325,9 +325,6 @@ class Game(BaseGame):
             elif self.state == GameState.LIVE:
                 self._logger.info("Game finished normally")
 
-                for player in self._players_with_unsent_army_stats:
-                    await self._process_army_stats_for_player(player)
-
                 if self.desyncs > 20:
                     await self.mark_invalid(ValidityState.TOO_MANY_DESYNCS)
                     return
@@ -347,6 +344,7 @@ class Game(BaseGame):
 
                 await self.persist_results()
                 await self.rate_game()
+                await self._process_pending_army_stats()
         except Exception as e:
             self._logger.exception("Error during game end: %s", e)
         finally:
