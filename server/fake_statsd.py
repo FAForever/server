6b4f47c0249ec085ac2752ff30a2771a3e985bec
Rackochad <33836535+Rackover@users.noreply.github.com>
2018-11-09 21:21:06 +0100

    Fixed memory leak when StatsD is disabled (#364)

---
+++ server/fake_statsd.py
@@ -0,0 +1,27 @@
+
+
+class DummyConnection:
+    def __init__(self):
+        pass
+
+    class timer:
+        def __init__(self, a):
+            pass
+
+        def __enter__(self):
+            pass
+
+        def __exit__(self, a, b, c):
+            pass
+
+    def gauge(self, a, b, delta=False):
+        pass
+
+    def incr(self, a):
+        pass
+
+    class unit:
+        def __init__(self):
+            pass
+
+
