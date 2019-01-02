eba208197c7f184c2bfda0169ce2f97cd0ae172d
Rackover <rackover@racknet.noip.me>
2019-01-02 11:19:01 +0000

    Control server now displays some stats, removed POST route to kick

--- server/control.py
+++ server/control.py
@@ -5,11 +5,13 @@ Tiny local-only http server for getting stats and performing various tasks
 import asyncio
 
 import msgpack
+import socket
 
 from aiohttp import web
 import logging
-from server import PlayerService, GameService, LobbyConnection
+from server import PlayerService, GameService, LobbyConnection, config
 from server.games import Game
+from json import dumps
 
 logger = logging.getLogger(__name__)
 
@@ -19,23 +21,16 @@ class ControlServer:
         self.game_service = game_service
         self.player_service = player_service
 
-    def games(self, request):
-        body = repr(list(map(Game.to_dict, self.game_service.all_games))).encode()
-        return web.Response(body=msgpack.packb(body), content_type='application/x-msgpack')
+    def games(self):
+        body = dumps(to_dict_list(self.game_service.all_games))
+        return web.Response(body=body.encode(), content_type='application/json')
 
-    def players(self, request):
-        body = list(map(lambda p: p.to_dict(), self.player_service.players.values()))
-        return web.Response(body=msgpack.packb(body), content_type='application/x-msgpack')
-
-    async def kick_player(self, request):
-        player = self.player_service.players[int(request.match_info.get('player_id'))]
-        assert isinstance(player.lobby_connection, LobbyConnection)
-        player.lobby_connection.kick()
-        return web.Response()
+    def players(self):
+        body = dumps(to_dict_list(self.player_service.players.values()))
+        return web.Response(body=body.encode(), content_type='application/json')
 
 
-@asyncio.coroutine
-def init(loop, player_service, game_service):
+async def init(loop, player_service, game_service):
     """
     Initialize the http control server
     """
@@ -43,8 +38,14 @@ def init(loop, player_service, game_service):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/games', ctrl_server.games)
     app.router.add_route('GET', '/players', ctrl_server.players)
-    app.router.add_route('POST', '/players/{player_id}', ctrl_server.kick_player)
 
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 4040)
-    logger.info("Control server listening on http://127.0.0.1:4040")
+    address = socket.gethostbyname(socket.gethostname())
+    port = config.CONTROL_SERVER_PORT
+
+    srv = await loop.create_server(app.make_handler(), address, port)
+    logger.info("Control server listening on http://%s:%s", address, port)
     return srv
+
+
+def to_dict_list(list_):
+    return list(map(lambda p: p.to_dict(), list_))
