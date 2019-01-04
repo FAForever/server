5679825a8631767a718f1bdc6fd7192148f434f2
Askaholic <askaholic907@gmail.com>
2019-01-03 19:43:25 -0900

    Add docstrings galore

--- server/geoip_service.py
+++ server/geoip_service.py
@@ -16,6 +16,14 @@ import traceback
 
 @with_logger
 class GeoIpService(object):
+    """
+        Service for managing the GeoIp database. This includes an asyncio crontab
+    which periodically checks if the current file is out of date. If it is, then
+    the service will try to download a new file from tue url in ``server.config``.
+
+        Provides an interface for getting data out of the database.
+    """
+
     def __init__(self):
         self.file_path = config.GEO_IP_DATABASE_PATH
 
@@ -24,7 +32,9 @@ class GeoIpService(object):
         asyncio.ensure_future(self.check_update_geoip_db())
 
     async def check_update_geoip_db(self) -> None:
-        """ Check if the geoip database is old and update it if so. """
+        """
+            Check if the geoip database is old and update it if so.
+        """
 
         self._logger.debug("Checking if geoip database needs updating")
         try:
@@ -33,10 +43,10 @@ class GeoIpService(object):
 
             if delta.days > config.GEO_IP_DATABASE_MAX_AGE_DAYS:
                 self._logger.info("Geoip database is out of date")
-                await self.do_update_geoip_db()
+                await self.download_geoip_db()
         except FileNotFoundError:  # pragma: no cover
             self._logger.warning("Geoip database is missing...")
-            await self.do_update_geoip_db()
+            await self.download_geoip_db()
         except asyncio.TimeoutError:  # pragma: no cover
             self._logger.warning("Failed to download database file! "
                                  "Check the network connection and try again")
@@ -47,19 +57,18 @@ class GeoIpService(object):
 
         self.load_db()
 
-    async def do_update_geoip_db(self) -> None:
+    async def download_geoip_db(self) -> None:
+        """
+            Download the geoip database to a file. If the downloaded file is not
+        a valid gzip file, then it does NOT overwrite the old file.
+        """
+
         self._logger.info("Downloading new geoip database")
+
         # Download new file to a temp location
-        chunk_size = 1024
         temp_file_path = "/tmp/geoip.mmdb.gz"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(config.GEO_IP_DATABASE_URL, timeout=60 * 20) as resp:
-                with open(temp_file_path, 'wb') as f:
-                    while True:
-                        chunk = await resp.content.read(chunk_size)
-                        if not chunk:
-                            break
-                        f.write(chunk)
+        await self._download_file(config.GEO_IP_DATABASE_URL, temp_file_path)
+
         # Unzip the archive and overwrite the old file
         try:
             with gzip.open(temp_file_path, 'rb') as f_in:
@@ -69,11 +78,36 @@ class GeoIpService(object):
             self._logger.warning("Failed to unzip downloaded file!")
         self._logger.info("New database download complete")
 
+    async def _download_file(self, url: str, file_path: str) -> None:
+        """
+            Download a file using aiohttp and save it to a file.
+
+            :param url: The url to download from
+            :param file_path: Path to save the file at
+        """
+
+        chunk_size = 1024
+        async with aiohttp.ClientSession() as session:
+            async with session.get(config.GEO_IP_DATABASE_URL, timeout=60 * 20) as resp:
+                with open(file_path, 'wb') as f:
+                    while True:
+                        chunk = await resp.content.read(chunk_size)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+
     def load_db(self) -> None:  # pragma: no cover
-        """ Loads the database into memory. """
+        """
+            Loads the database into memory.
+        """
+
         self.db = geoip2.database.Reader(self.file_path)
 
     def country(self, address: str) -> str:
+        """
+            Look up an ip address in the db and return it's country code.
+        """
+
         try:
             return str(self.db.country(address).country.iso_code)
         except geoip2.errors.AddressNotFoundError:
