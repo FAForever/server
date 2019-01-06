fbb1600a5c57d52761d83139b8dce4955123298e
Rackover <rackover@racknet.noip.me>
2019-01-06 11:19:43 +0000

    Added default title for titleless games

--- server/lobbyconnection.py
+++ server/lobbyconnection.py
@@ -772,7 +772,8 @@ class LobbyConnection:
 
         assert isinstance(self.player, Player)
 
-        title = html.escape(message.get('title', '') or '')
+        default_title = "%s' game".format(self.player.login)
+        title = html.escape(message.get('title') or default_title)
         port = message.get('gameport')
         visibility = VisibilityState.from_string(message.get('visibility'))
         if not isinstance(visibility, VisibilityState):
