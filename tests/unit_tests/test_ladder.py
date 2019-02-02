d19a7f9d5b7be6a24d08184f2726bd0cfce785c4
Askaholic <askaholic907@gmail.com>
2019-02-01 20:20:32 -0900

    Add test for excluding last played maps

--- tests/unit_tests/test_ladder.py
+++ tests/unit_tests/test_ladder.py
@@ -1,20 +1,24 @@
+from datetime import datetime
 from unittest import mock
 
 import pytest
-
-from server import GameStatsService, GameService, LadderService
+from server import GameService, GameStatsService, LadderService
 from server.players import Player
 from tests import CoroMock
 
 
 @pytest.fixture
-def ladder_service(game_service: GameService, game_stats_service: GameStatsService):
+def ladder_service(game_service: GameService, game_stats_service: GameStatsService, db_engine):
+    # db_engine needed to ensure the engine is created globally
     return LadderService(game_service, game_stats_service)
 
 
 async def test_start_game(ladder_service: LadderService, game_service: GameService):
     p1 = mock.create_autospec(Player('Dostya', id=1))
     p2 = mock.create_autospec(Player('Rhiza', id=2))
+
+    p1.id = 1
+    p2.id = 2
     game_service.ladder_maps = [(1, 'scmp_007', 'maps/scmp_007.zip')]
 
     with mock.patch('asyncio.sleep', CoroMock()):
@@ -31,3 +35,26 @@ def test_inform_player(ladder_service: LadderService):
     ladder_service.inform_player(p1)
 
     assert p1.lobby_connection.sendJSON.called
+
+
+async def test_choose_map(ladder_service: LadderService):
+    ladder_service.get_ladder_history = CoroMock(
+        return_value=[
+            (1, datetime.now()),
+            (2, datetime.now()),
+            (3, datetime.now())
+        ]
+    )
+
+    ladder_service.game_service.ladder_maps = [
+        (1, "some_map", "maps/some_map.v001.zip"),
+        (2, "some_map", "maps/some_map.v001.zip"),
+        (3, "some_map", "maps/some_map.v001.zip"),
+        (4, "CHOOSE_ME", "maps/choose_me.v001.zip"),
+    ]
+
+    chosen_map = await ladder_service.choose_map([None])
+
+    # Make the probability very low that the test passes because we got lucky
+    for _ in range(20):
+        assert chosen_map == (4, "CHOOSE_ME", "maps/choose_me.v001.zip")
