2d3b25485705c47b421a29bf0e2fb88dcdcf32a5
Askaholic <askaholic907@gmail.com>
2019-03-07 11:04:55 -0900

    Remove validity check in on_game_end

--- server/games/game.py
+++ server/games/game.py
@@ -434,9 +434,6 @@ class Game(BaseGame):
             elif self.state == GameState.LIVE:
                 self._logger.info("Game finished normally")
 
-                if self.validity is not ValidityState.VALID:
-                    return
-
                 if self.desyncs > 20:
                     await self.mark_invalid(ValidityState.TOO_MANY_DESYNCS)
                     return
