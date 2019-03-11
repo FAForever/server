10f6835d0c5e286e62ba2535709ef0463992c784
Askaholic <askaholic907@gmail.com>
2019-03-11 12:27:25 -0800

    Use await syntax

--- tests/integration_tests/test_server.py
+++ tests/integration_tests/test_server.py
@@ -33,37 +33,37 @@ def lobby_server(request, loop, db_engine, player_service, game_service, geoip_s
     return ctx
 
 
-@asyncio.coroutine
-def connect_client(server):
-    return QDataStreamProtocol(*(yield from asyncio.open_connection(*server.sockets[0].getsockname())))
+async def connect_client(server):
+    return QDataStreamProtocol(
+        *(await asyncio.open_connection(*server.sockets[0].getsockname()))
+    )
 
 
-@asyncio.coroutine
-def get_session(proto):
+async def get_session(proto):
     proto.send_message({'command': 'ask_session', 'user_agent': 'faf-client', 'version': '0.11.16'})
-    yield from proto.drain()
-    msg = yield from proto.read_message()
+    await proto.drain()
+    msg = await proto.read_message()
+
     return msg['session']
 
 
-@asyncio.coroutine
-def perform_login(proto, credentials):
+async def perform_login(proto, credentials):
     login, pw = credentials
     pw_hash = hashlib.sha256(pw.encode('utf-8'))
-    proto.send_message({'command': 'hello',
-                        'version': '1.0.0-dev',
-                        'user_agent': 'faf-client',
-                        'login': login,
-                        'password': pw_hash.hexdigest(),
-                        'unique_id': 'some_id'
-                        })
-    yield from proto.drain()
+    proto.send_message({
+        'command': 'hello',
+        'version': '1.0.0-dev',
+        'user_agent': 'faf-client',
+        'login': login,
+        'password': pw_hash.hexdigest(),
+        'unique_id': 'some_id'
+    })
+    await proto.drain()
 
 
-@asyncio.coroutine
-def read_until(proto, pred):
+async def read_until(proto, pred):
     while True:
-        msg = yield from proto.read_message()
+        msg = await proto.read_message()
         try:
             if pred(msg):
                 return msg
@@ -82,12 +82,11 @@ async def test_server_invalid_login(loop, lobby_server):
     proto.close()
 
 
-@asyncio.coroutine
 @slow
-def test_server_valid_login(loop, lobby_server):
-    proto = yield from connect_client(lobby_server)
-    yield from perform_login(proto, ('test', 'test_password'))
-    msg = yield from proto.read_message()
+async def test_server_valid_login(loop, lobby_server):
+    proto = await connect_client(lobby_server)
+    await perform_login(proto, ('test', 'test_password'))
+    msg = await proto.read_message()
     assert msg == {'command': 'welcome',
                    'me': {'clan': '678',
                           'country': '',
@@ -100,20 +99,20 @@ def test_server_valid_login(loop, lobby_server):
                    'login': 'test'}
     lobby_server.close()
     proto.close()
-    yield from lobby_server.wait_closed()
+    await lobby_server.wait_closed()
 
 
-@asyncio.coroutine
-def test_player_info_broadcast(loop, lobby_server):
-    p1 = yield from connect_client(lobby_server)
-    p2 = yield from connect_client(lobby_server)
+async def test_player_info_broadcast(loop, lobby_server):
+    p1 = await connect_client(lobby_server)
+    p2 = await connect_client(lobby_server)
 
-    yield from perform_login(p1, ('test', 'test_password'))
-    yield from perform_login(p2, ('Rhiza', 'puff_the_magic_dragon'))
+    await perform_login(p1, ('test', 'test_password'))
+    await perform_login(p2, ('Rhiza', 'puff_the_magic_dragon'))
 
-    yield from read_until(p2,
-                          lambda m: 'player_info' in m.values()
-                                    and any(map(lambda d: ('login', 'test') in d.items(), m['players'])))
+    await read_until(
+        p2, lambda m: 'player_info' in m.values()
+        and any(map(lambda d: ('login', 'test') in d.items(), m['players']))
+    )
     p1.close()
     p2.close()
 
