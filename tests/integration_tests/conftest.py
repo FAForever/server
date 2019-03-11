9d369f98d461e26f61b0f140ac8292b89e07b3b1
Askaholic <askaholic907@gmail.com>
2019-03-11 12:27:33 -0800

    Move imports

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,16 +1,16 @@
 from unittest import mock
 
 import pytest
+from server import GameService, PlayerService
 
 
 @pytest.fixture
 def mock_players(db_engine):
-    from server import PlayerService
     m = mock.create_autospec(PlayerService())
     m.client_version_info = (0, None)
     return m
 
+
 @pytest.fixture
 def mock_games(mock_players):
-    from server import GameService
     return mock.create_autospec(GameService(mock_players))
