5fa8dc067bd3364e8a5a15ab128ed04fa99a39e9
Askaholic <askaholic907@gmail.com>
2019-05-02 11:52:05 -0800

    Add some tests for PlayerService (#425)
    * Add test for fetch_player_data
    
    * Rewrite fetch_player_data to get all data from a single query
    
    * Move avatar selection into fetch_player_data
    
    * Cleaned up joins by adding login table
    
    * Remove extra indents

--- server/db/models.py
+++ server/db/models.py
@@ -5,6 +5,26 @@ from ..games.game import Victory
 
 metadata = MetaData()
 
+avatars = Table(
+    'avatars', metadata,
+    Column('id',            Integer,    primary_key=True),
+    Column('idUser',        Integer,    ForeignKey('login.id')),
+    Column('idAvatar',      Integer,    ForeignKey('avatars_list.id')),
+    Column('selected',      Boolean,    nullable=False),
+    Column('expires_at',    TIMESTAMP),
+    Column('create_time',   TIMESTAMP,  nullable=False),
+    Column('update_time',   TIMESTAMP,  nullable=False)
+)
+
+avatars_list = Table(
+    'avatars_list', metadata,
+    Column('id',            Integer,    primary_key=True),
+    Column('url',           String,     nullable=False),
+    Column('tooltip',       String),
+    Column('create_time',   TIMESTAMP,  nullable=False),
+    Column('update_time',   TIMESTAMP,  nullable=False)
+)
+
 ban = Table(
     'ban', metadata,
     Column('id',            Integer,    primary_key=True),
@@ -16,6 +36,28 @@ ban = Table(
     Column('create_time',   TIMESTAMP,  nullable=False),
     Column('update_time',   TIMESTAMP,  nullable=False),
     Column('report_id',     Integer,    ForeignKey('moderation_report.id'))
+)
+
+clan = Table(
+    'clan', metadata,
+    Column('id',            Integer,    primary_key=True),
+    Column('name',          String,     nullable=False),
+    Column('tag',           String,     nullable=False),
+    Column('founder_id',    Integer,    ForeignKey('login.id')),
+    Column('leader_id',     Integer,    ForeignKey('login.id')),
+    Column('description',   String),
+    Column('tag_color',     String),
+    Column('create_time',   TIMESTAMP,  nullable=False),
+    Column('update_time',   TIMESTAMP,  nullable=False)
+)
+
+clan_membership = Table(
+    'clan_membership', metadata,
+    Column('id',            Integer,    primary_key=True),
+    Column('clan_id',       Integer,    ForeignKey('clan.id'),   nullable=False),
+    Column('player_id',     Integer,    ForeignKey('login.id'),  nullable=False),
+    Column('create_time',   TIMESTAMP,  nullable=False),
+    Column('update_time',   TIMESTAMP,  nullable=False)
 )
 
 friends_and_foes = Table(
@@ -41,20 +83,20 @@ game_featuredMods = Table(
 
 game_player_stats = Table(
     'game_player_stats', metadata,
-    Column('id',        Integer, primary_key=True),
-    Column('gameId',    Integer, ForeignKey('game_stats.id'), nullable=False),
-    Column('playerId',  Integer, nullable=False),
-    Column('AI',        Boolean, nullable=False),
-    Column('faction',   Integer, nullable=False),
-    Column('color',     Integer, nullable=False),
-    Column('team',      Integer, nullable=False),
-    Column('place',     Integer, nullable=False),
-    Column('mean',      Float,   nullable=False),
-    Column('deviation', Float,   nullable=False),
-    Column('after_mean', Float),
+    Column('id',            Integer, primary_key=True),
+    Column('gameId',        Integer, ForeignKey('game_stats.id'), nullable=False),
+    Column('playerId',      Integer, ForeignKey('login.id'),      nullable=False),
+    Column('AI',            Boolean, nullable=False),
+    Column('faction',       Integer, nullable=False),
+    Column('color',         Integer, nullable=False),
+    Column('team',          Integer, nullable=False),
+    Column('place',         Integer, nullable=False),
+    Column('mean',          Float,   nullable=False),
+    Column('deviation',     Float,   nullable=False),
+    Column('after_mean',    Float),
     Column('after_deviation', Float),
-    Column('score',     Integer),
-    Column('scoreTime', TIMESTAMP),
+    Column('score',         Integer),
+    Column('scoreTime',     TIMESTAMP),
 )
 
 game_stats = Table(
@@ -64,8 +106,41 @@ game_stats = Table(
     Column('endTime',   TIMESTAMP),
     Column('gameType',  Enum(Victory), nullable=False),
     Column('gameMod',   Integer, ForeignKey('game_featuredMods.id'), nullable=False),
-    Column('host',      Integer,                nullable=False),
+    Column('host',      Integer,    nullable=False),
     Column('mapId',     Integer),
-    Column('gameName',  String,                 nullable=False),
-    Column('validity',  Integer,                nullable=False),
+    Column('gameName',  String,     nullable=False),
+    Column('validity',  Integer,    nullable=False),
+)
+
+global_rating = Table(
+    'global_rating', metadata,
+    Column('id',        Integer,    ForeignKey('login.id'), primary_key=True),
+    Column('mean',      Float),
+    Column('deviation', Float),
+    Column('numGames',  Integer,    nullable=False),
+    Column('is_active', Boolean,    nullable=False),
+)
+
+login = Table(
+    'login', metadata,
+    Column('id',            Integer,    primary_key=True),
+    Column('login',         String,     nullable=False, unique=True),
+    Column('password',      String,     nullable=False),
+    Column('email',         String,     nullable=False, unique=True),
+    Column('ip',            String),
+    Column('steamid',       Integer,     unique=True),
+    Column('create_time',   TIMESTAMP,  nullable=False),
+    Column('update_time',   TIMESTAMP,  nullable=False),
+    Column('user_agent',    String),
+    Column('last_login',    TIMESTAMP)
+)
+
+ladder1v1_rating = Table(
+    'ladder1v1_rating', metadata,
+    Column('id',            Integer,    ForeignKey('login.id'), primary_key=True),
+    Column('mean',          Float),
+    Column('deviation',     Float),
+    Column('numGames',      Integer,    nullable=False),
+    Column('winGames',      Integer,    nullable=False),
+    Column('is_active',     Boolean,    nullable=False)
 )
