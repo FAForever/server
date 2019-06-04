adee1f089c674b4924cfa05fbe0fed8c36a28c3f
louve <rackover@racknet.noip.me>
2019-06-04 07:15:46 +0000

    Updated styling again

--- tests/unit_tests/test_ice.py
+++ tests/unit_tests/test_ice.py
@@ -37,7 +37,7 @@ def test_coturn_tokens(coturn_hmac, coturn_hosts, coturn_credentials):
     comparison_list = []
     for coturn_host,coturn_cred in zip(coturn_hosts, coturn_credentials):
         comparison_list.append(
-            {"host":coturn_host, "cred":coturn_cred}
+            {"host": coturn_host, "cred": coturn_cred}
         )
 
     for i in range(0, len(servers)):
@@ -46,11 +46,11 @@ def test_coturn_tokens(coturn_hmac, coturn_hosts, coturn_credentials):
         credential = comparison_list[i]['cred']
         assert server['credentialType'] == 'token'
         assert server['urls'] == \
-                [
-                    f"turn:{host}?transport=tcp",
-                    f"turn:{host}?transport=udp",
-                    f"stun:{host}"
-                ]
+            [
+                f"turn:{host}?transport=tcp",
+                f"turn:{host}?transport=udp",
+                f"stun:{host}"
+            ]
 
         assert server['credential'] == credential
         assert server['username'] == '124456:faf-test'
