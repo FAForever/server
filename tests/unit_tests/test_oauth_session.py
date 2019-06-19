2bd3b55b13381209bf00c64722e8bf6f5bf0db8c
Askaholic <askaholic907@gmail.com>
2019-06-19 19:40:28 +0200

    Implement OAuth2 authentication with aiohttp to prevent blocking (#433)
    * Implement oauth2 using aiohttp
    
    * Connect to policy server using aiohttp not requests
    
    * Modify ApiAccessor for aiohttp response objects
    
    * Use .startswith for https check
    
    * Test that missing tokens are checked
    
    * Add test for SessionManager
    
    * Add error handling in event and achievemnt service
    
    * Add tests for Oauth2Session
    
    * Add tests for more error conditions
    
    * Let pytest handle skiping slow tests
    
    * Replace call to deprecated aiohttp api
    
    * Add test for policy server connection code
    
    * Use `async with` for aiohttp session
    
    * Add more tests for check_policy_conformity
    
    This actually uncovered a bug which prevented fradulent logins from 
    receiving an auto-ban
    
    * Add test for player signing in from two machines at the same time

---
+++ tests/unit_tests/test_oauth_session.py
@@ -0,0 +1,17 @@
+import pytest
+from oauthlib.oauth2.rfc6749.errors import (InsecureTransportError,
+                                            MissingTokenError)
+from server.api.oauth_session import OAuth2Session
+
+
+async def test_error_conditions():
+    oauth_session = OAuth2Session(
+        client_id="client_id",
+        client_secret="client_secret",
+        token_url='http://some_url'
+    )
+    with pytest.raises(InsecureTransportError):
+        await oauth_session.fetch_token()
+
+    with pytest.raises(MissingTokenError):
+        await oauth_session.request('GET', 'http://some_other_url')
