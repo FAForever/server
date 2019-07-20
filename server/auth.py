0fa4ca93a45392da816c132df5640ee16ed282df
Askaholic <askaholic907@gmail.com>
2019-07-19 20:39:16 -0800

    Cleanup auth module

--- server/auth.py
+++ server/auth.py
@@ -7,7 +7,7 @@ from .exceptions import AuthenticationError
 
 
 class AuthModule(Module):
-    KEY_HEADER = '-----BEGIN PUBLIC KEY-----'
+    KEY_HEADER = "-----BEGIN PUBLIC KEY-----"
 
     def __init__(self) -> None:
         super(AuthModule, self).__init__()
@@ -26,19 +26,11 @@ mod = AuthModule()
 
 @mod.route("auth")
 async def handle_auth(conn, message):
-    token = message['token']
+    token = message["token"]
     try:
         token = jwt.decode(token, mod.pub_key, algorithms="RS256")
-    except jwt.InvalidTokenError:
+        user_id = token["user_id"]
+    except (InvalidTokenError, KeyError):
         raise AuthenticationError("Token signature was invalid")
-    user_id = token['user_id']
+
     await conn.on_player_login(user_id, message)
-
-
-def valid_token(token: str) -> bool:
-    assert API_JWT_PUBLIC_KEY, "Pub key must be set to use oauth login"
-    try:
-        jwt.decode(token, API_JWT_PUBLIC_KEY, algorithms='RS256')
-        return True
-    except InvalidTokenError:
-        return False
