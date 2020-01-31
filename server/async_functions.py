4ffa009737231f3a5d53144ad7e7b0f51ea2cddf
Askaholic <askaholic907@gmail.com>
2020-01-30 18:30:31 -0900

    Fix gather_without_exception subclass behavior

--- server/async_functions.py
+++ server/async_functions.py
@@ -2,18 +2,23 @@
 Some helper functions for common async tasks.
 """
 import asyncio
-from typing import List
+from typing import Any, List
 
 
 async def gather_without_exceptions(
     tasks: List[asyncio.Task],
-    *exc_classes: type,
-) -> None:
+    *exceptions: type,
+) -> List[Any]:
     """
     Call gather on a list of tasks, raising the first exception that dosen't
     match any of the specified exception classes.
     """
     results = await asyncio.gather(*tasks, return_exceptions=True)
     for result in results:
-        if isinstance(result, Exception) and result.__class__ not in exc_classes:
-            raise result
+        if isinstance(result, Exception):
+            # Check if this exception is an instance (maybe subclass) that
+            # should be ignored
+            for exc_type in exceptions:
+                if not isinstance(result, exc_type):
+                    raise result
+    return results
