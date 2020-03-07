91c1b8c8952ed8df0afb944039bc28a3d97c1cbf
Clemens Borys <clemens@borys.dk>
2020-03-07 13:44:57 +0100

    patcher.py only

--- patcher.py
+++ patcher.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     source = sys.argv[1]
     target = sys.argv[2]
     patchname = sys.argv[3]
-   
+
     patchfile = open(patchname, 'w')
-    subprocess.call(['xdelta3', '-I0','-B134217728', '-P16777216', '-W16777216', '-9', '-s', source, target], stdout = patchfile)
+    subprocess.call(['xdelta3', '-I0', '-B134217728', '-P16777216', '-W16777216', '-9', '-s', source, target], stdout=patchfile)
     patchfile.close()
