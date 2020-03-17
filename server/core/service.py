50922450559891e6e567260443648eac8c381b65
Askaholic <askaholic907@gmail.com>
2020-03-17 15:47:20 -0800

    Add some docstrings for service

--- server/core/service.py
+++ server/core/service.py
@@ -2,10 +2,15 @@ from abc import abstractmethod
 
 
 class Service(object):
+    """
+    All services should inherit from this class.
+
+    Services are singleton objects which manage some server task.
+    """
+
     @abstractmethod
     async def initialize(self) -> None:
         """
-        Called once before the service is used to run initial asynchronus tasks
-        such as loading data from the database.
+        Called once while the server is starting.
         """
         pass
