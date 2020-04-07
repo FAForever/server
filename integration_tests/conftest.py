0840f7949f34830b933d6cccb7f6b453410a76bb
Askaholic <askaholic907@gmail.com>
2020-04-07 15:10:49 -0800

    Disconnect clients automatically after tests

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -3,13 +3,18 @@ import pytest
 from .fafclient import FAFClient
 
 
-@pytest.fixture(scope="session")
-def test_client(request):
+@pytest.fixture(scope="function")
+async def test_client(request):
+    clients = []
     """Create a new client connected to the test server"""
     async def connect(username, password="foo"):
         client = FAFClient()
+        clients.append(client)
         await client.connect("test.faforever.com", 8001)
         msg = await client.login(username, password)
         return client, msg
 
-    return connect
+    yield connect
+
+    for client in clients:
+        await client.close()
