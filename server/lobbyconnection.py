5b062bc4306f2a81f67e3270866f5624a04d45f4
Askaholic <askaholic907@gmail.com>
2020-06-29 13:42:37 -0800

    Move definition of 'campaigns'

--- server/lobbyconnection.py
+++ server/lobbyconnection.py
@@ -201,6 +201,13 @@ class LobbyConnection:
             result = await conn.execute(select([coop_map]))
 
             maps = []
+            campaigns = [
+                "FA Campaign",
+                "Aeon Vanilla Campaign",
+                "Cybran Vanilla Campaign",
+                "UEF Vanilla Campaign",
+                "Custom Missions"
+            ]
             async for row in result:
                 json_to_send = {
                     "command": "coop_info",
@@ -209,13 +216,6 @@ class LobbyConnection:
                     "filename": row["filename"],
                     "featured_mod": "coop"
                 }
-                campaigns = [
-                    "FA Campaign",
-                    "Aeon Vanilla Campaign",
-                    "Cybran Vanilla Campaign",
-                    "UEF Vanilla Campaign",
-                    "Custom Missions"
-                ]
                 if row["type"] < len(campaigns):
                     json_to_send["type"] = campaigns[row["type"]]
                 else:
