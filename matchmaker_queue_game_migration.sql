ccd4654cde3efa5d85cc93cc7bd46a6913265e14
Clemens Borys <clemens@borys.dk>
2020-09-03 13:56:59 +0200

    Add matchmaker_queue_game table

---
+++ matchmaker_queue_game_migration.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS matchmaker_queue_game;
+
+CREATE TABLE matchmaker_queue_game
+(
+  matchmaker_queue_id INT(10) UNSIGNED NOT NULL,
+  game_stats_id       INT(10) UNSIGNED NOT NULL,
+  FOREIGN KEY (matchmaker_queue_id) REFERENCES matchmaker_queue (id),
+  FOREIGN KEY (game_stats_id) REFERENCES game_stats (id),
+  PRIMARY KEY (game_stats_id, matchmaker_queue_id)
+);
