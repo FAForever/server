1a50aa0d37da64b81efc5285bb820a72d528fcf4
Askaholic <askaholic907@gmail.com>
2021-04-17 16:32:21 -0800

    Setup rabbitmq in github actions and run rabbitmq tests

---
+++ .github/workflows/scripts/init-rabbitmq.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+MAX_WAIT=60 # max. 1 minute waiting time in loop before timeout
+RABBITMQ_PID_FILE=/var/lib/rabbitmq/pid
+
+RABBITMQ_LOBBYSERVER_USER=faf-lobby
+RABBITMQ_LOBBYSERVER_PASS=banana
+RABBITMQ_LOBBYSERVER_VHOST=/faf-lobby
+
+# Create RabbitMQ users
+docker exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
+
+docker exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_LOBBYSERVER_VHOST}"
+docker exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_LOBBYSERVER_USER}" "${RABBITMQ_LOBBYSERVER_PASS}"
+docker exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_LOBBYSERVER_VHOST}" "${RABBITMQ_LOBBYSERVER_USER}" ".*" ".*" ".*"
