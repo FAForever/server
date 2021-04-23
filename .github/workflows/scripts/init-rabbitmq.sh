6a474c6f73873a377bb08ed8dfbb3aaa66649f25
Askaholic <askaholic907@gmail.com>
2021-04-23 09:56:28 -0800

    Rename MAX_WAIT to MAX_WAIT_SECONDS

--- .github/workflows/scripts/init-rabbitmq.sh
+++ .github/workflows/scripts/init-rabbitmq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MAX_WAIT=60 # max. 1 minute waiting time in loop before timeout
+MAX_WAIT_SECONDS=60
 RABBITMQ_PID_FILE=/var/lib/rabbitmq/pid
 
 RABBITMQ_LOBBYSERVER_USER=faf-python-server
@@ -8,7 +8,7 @@ RABBITMQ_LOBBYSERVER_PASS=banana
 RABBITMQ_LOBBYSERVER_VHOST=/faf-core
 
 # Create RabbitMQ users
-docker exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
+docker exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT_SECONDS} "${RABBITMQ_PID_FILE}"
 
 docker exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_LOBBYSERVER_VHOST}"
 docker exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_LOBBYSERVER_USER}" "${RABBITMQ_LOBBYSERVER_PASS}"
