541c2b62899684a7fccb1695b7514e2d0332e8d6
Sheikah45 <Sheikah450@gmail.com>
2021-06-07 22:31:08 -0400

    log hydra JWKS

--- server/oauth_service.py
+++ server/oauth_service.py
@@ -23,6 +23,7 @@ class OAuthService(Service, name="oauth_service"):
         """
         Get the latest jwks from the hydra endpoint
         """
+        self._logger.info(config.HYDRA_JWKS_URI)
         async with aiohttp.ClientSession(raise_for_status=True) as session:
             async with session.get(config.HYDRA_JWKS_URI) as resp:
                 jwks = await resp.json()
