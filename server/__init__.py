58c5887e386789f406532fba38e7c49d7e73642a
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2021-06-12 16:09:08 -0400

    Feature/#440 hydra token login (#784)
    * Refactor `command_hello`
    
    * Added `command_auth` for logging in with an API token
    
    * Refactor hash computation in update_irc_password
    
    * Choose IRC password as hex encoded string to avoid '/' character
    
    * Use lowercase for resolved pub key value so it is not considered a valid config key
    
    * Use separate variables for pub key file vs direct pub key
    
    * More cleanup
    
    * Use hydra token service for authentication
    
    * import jwt algorithms
    
    * fix import order
    
    * Use pyjwk client
    
    * change import
    
    * Use older jwt version without jwkClient due to twilio constraints
    
    update dependencies
    
    * Use older version of twilio to get around pyjwt version lock
    
    * Fix cron task
    
    * Rename to OauthService
    
    * log out decoded token
    
    * correct decoded token field
    
    * change sub to int
    
    * Use aiohttp to get jwks
    
    * Turn off audience verification
    
    * Fix tests
    
    * Revert db name change
    
    * use mocks properly
    
    * sort imports
    
    * fix tests
    
    * Fix codacy where possible
    
    * Address formatting
    
    * Remove unused import
    
    * only update keys once fully retrieved
    
    * Add detection for player id not in database
    
    * log login method in metrics
    
    * Make two labels for the user_logins
    
    * make kid string its own fixture, log user not in db, use dict comprehension
    
    * Fix test and formatting
    
    * Pipenv lock from linux
    
    * Change to prod hydra
    
    * Fix comment
    
    * Fix test formatting
    
    Co-authored-by: Askaholic <askaholic907@gmail.com>

--- server/__init__.py
+++ server/__init__.py
@@ -108,6 +108,7 @@ from .ice_servers.nts import TwilioNTS
 from .ladder_service import LadderService
 from .lobbyconnection import LobbyConnection
 from .message_queue_service import MessageQueueService
+from .oauth_service import OAuthService
 from .party_service import PartyService
 from .player_service import PlayerService
 from .protocol import Protocol, QDataStreamProtocol
@@ -129,6 +130,7 @@ __all__ = (
     "GeoIpService",
     "LadderService",
     "MessageQueueService",
+    "OAuthService",
     "PartyService",
     "PlayerService",
     "RatingService",
@@ -189,7 +191,8 @@ class ServerInstance(object):
             nts_client=twilio_nts,
             players=self.services["player_service"],
             ladder_service=self.services["ladder_service"],
-            party_service=self.services["party_service"]
+            party_service=self.services["party_service"],
+            oauth_service=self.services["oauth_service"]
         )
 
     def write_broadcast(
