9c1e3bd05ee33f5bde381e03a7842527c47d7013
KaukaHan <58892193+KaukaHan@users.noreply.github.com>
2022-03-12 09:07:19 +0100

    issue/#812 add player ids to game_info (#864)
    * put playerids into teams
    
    * rename ´player_ids´ field to ´teams_ids´
    
    * replace flat lists and add tests
    
    * fix tests
    
    * fix typo
    
    * fix test
    
    * fix test
    
    * fix code style

--- server/games/game.py
+++ server/games/game.py
@@ -904,6 +904,16 @@ class Game:
             "rating_min": self.displayed_rating_range.lo,
             "rating_max": self.displayed_rating_range.hi,
             "enforce_rating_range": self.enforce_rating_range,
+            "teams_ids": [
+                {
+                    "team_id": team,
+                    "player_ids": [
+                        player.id for player in connected_players
+                        if self.get_player_option(player.id, "Team") == team
+                    ]
+                }
+                for team in self.teams if team is not None
+            ],
             "teams": {
                 team: [
                     player.login for player in connected_players
