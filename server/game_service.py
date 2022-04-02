083a6fac978b91126daca5c0b89a8f761c02f51f
Askaholic <askaholic@protonmail.com>
2022-04-01 23:01:06 -0800

    Use python default and add unit test

--- server/game_service.py
+++ server/game_service.py
@@ -82,7 +82,8 @@ class GameService(Service):
             # doing LAST_UPDATE_ID to get the id number, and then doing an UPDATE when the actual
             # data to go into the row becomes available: we now only do a single insert for each
             # game, and don't end up with 800,000 junk rows in the database.
-            self.game_id_counter = await conn.scalar("SELECT COALESCE(MAX(id),0) FROM game_stats")
+            sql = "SELECT MAX(id) FROM game_stats"
+            self.game_id_counter = await conn.scalar(sql) or 0
 
     async def update_data(self):
         """
