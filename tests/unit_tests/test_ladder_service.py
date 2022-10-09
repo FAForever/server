b7cca92fbd9e6ad7730679b434f59d126d6233e1
maudlin27 <95254039+maudlin27@users.noreply.github.com>
2022-10-09 07:53:00 +0100

    Syntax fix

--- tests/unit_tests/test_ladder_service.py
+++ tests/unit_tests/test_ladder_service.py
@@ -274,7 +274,7 @@ async def test_start_game_timeout_on_send(
         mock.call({
             "command": "notice",
             "style": "info",
-            "text": You have caused a matchmaking connection failure 1 time(s). "
+            "text": "You have caused a matchmaking connection failure 1 time(s). "
                     "Multiple failures result in temporary time-outs from matchmaker. "
                     "Please seek support on the forums or discord for persistent issues."
         })
