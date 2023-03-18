6bc37c4c4d093301795e0f96a8397da577ae552c
Askaholic <askaholic@protonmail.com>
2023-03-18 01:40:44 +0000

    Fix docker build (#955)
    * Install aiomysql in normal mode (not editable)
    
    * Sort dependency list
    
    * Rename server.py to avoid naming conflict with server package
    
    * Set git dir in setup.py get_version
    
    This should allow the correct version tag to be found even when the pip 
    install command is run from a different directory.
    
    * Change version identifiers to comply with setuptools formats
    
    * Update dependencies
    
    * Rewrite Dockerfile to multi-stage with builder
    
    * Add missing __init__.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import re
 import subprocess
 from distutils.core import setup
+from pathlib import Path
 
 from setuptools import find_packages
 
@@ -9,15 +10,18 @@ import server
 
 def get_version() -> str:
     output = subprocess.run(
-        ["git", "describe", "--tags"],
+        [
+            "git", "--git-dir", Path(__file__).parent / ".git",
+            "describe", "--tags"
+        ],
         capture_output=True
     ).stdout.decode().strip().split("-")
     # Output is either v1.3.5 if the tag points to the current commit or
     # something like this v1.3.5-11-g3b467ad if it doesn't
 
-    version = ".".join(re.findall(r"\d+", output[0])) or "dev"
+    version = ".".join(re.findall(r"\d+", output[0])) or "0.dev"
     if len(output) > 1:
-        return f"{version}-{output[-1]}"
+        return f"{version}+{output[-1]}"
     else:
         return version
 
