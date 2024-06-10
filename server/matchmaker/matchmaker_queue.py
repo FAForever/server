f187dafaebba650c6ebe52462ff1596544a1ab29
BlackYps <jonas-fb@gmx.de>
2024-06-10 23:14:29 +0200

    Sort ratings list

--- server/matchmaker/matchmaker_queue.py
+++ server/matchmaker/matchmaker_queue.py
@@ -292,7 +292,7 @@ class MatchmakerQueue:
                 ndigits=2
             ),
             "num_players": self.num_players,
-            "ratings": list(ratings),
+            "ratings": sorted(ratings),
             # DEPRECATED
             "boundary_80s": [search.boundary_80 for search in self._queue.keys()],
             # DEPRECATED
