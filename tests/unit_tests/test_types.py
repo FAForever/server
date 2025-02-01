c541105b13612576c94b101475b9e93613ee25f1
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2025-02-01 16:49:20 -0500

    Ensure all generated maps are ranked (#1039)
    * Expand generated map regex
    
    * Add test for `is_neroxis_map`
    
    ---------
    
    Co-authored-by: Askaholic <askaholic@protonmail.com>

--- tests/unit_tests/test_types.py
+++ tests/unit_tests/test_types.py
@@ -1,4 +1,4 @@
-from server.types import Address
+from server.types import Address, NeroxisGeneratedMap
 
 
 def test_address_from_string():
@@ -11,3 +11,15 @@ def test_address_from_string_with_scheme():
     address = Address.from_string("http://localhost:4000")
 
     assert address == Address("http://localhost", 4000)
+
+
+def test_is_neroxis_map():
+    assert NeroxisGeneratedMap.is_neroxis_map(
+        "neroxis_map_generator_1.13.0_htvbrglnjszew_baeaeaa_aaaaaadhlwmjq",
+    ) is True
+    assert NeroxisGeneratedMap.is_neroxis_map(
+        "neroxis_map_generator_1.13.0_zxgxph43rfbj4_aycaeaa_aaaaaadhlwuas",
+    ) is True
+    assert NeroxisGeneratedMap.is_neroxis_map(
+        "neroxis_map_generator_1.13.0_avkycu6ce63e2_baeaeaa_aaaaaadhlxdhe",
+    ) is True
