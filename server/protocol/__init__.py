fac8a5b1d17d2b009c2070bb1c03b85d56f2ed75
Askaholic <askaholic@protonmail.com>
2025-03-14 13:57:36 -0400

    Improve README (#1055)
    * Update readme to better link to the docs and other relevant FAF repos
    
    * Improve readme and docs for network protocol

--- server/protocol/__init__.py
+++ server/protocol/__init__.py
@@ -1,5 +1,12 @@
 """
 Protocol format definitions
+
+Every message sent between the lobby server and a client is expected to
+deserialize to a python dictionary. The structure of that dictionary depends on
+the application logic (see `server.lobbyconnection`).
+
+This module defines the classes that handle the wire format, i.e. how messages
+are serialized to bytes and sent across the network.
 """
 
 from .gpgnet import GpgNetClientProtocol, GpgNetServerProtocol
